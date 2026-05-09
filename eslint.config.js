d34c323b7516d850f9834c9cc9319a1c6c9a1692
Claude Perrin <klodr@users.noreply.github.com>
2026-05-09 17:16:50 +0200

    chore(eslint): add eslint-plugin-promise (flat/recommended)

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@
 
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import promise from "eslint-plugin-promise";
 import prettier from "eslint-config-prettier";
 
 const __dirname = import.meta.dirname;
@@ -22,6 +23,7 @@ export default tseslint.config(
   },
   js.configs.recommended,
   ...tseslint.configs.strict,
+  promise.configs["flat/recommended"],
   prettier,
   {
     languageOptions: {
